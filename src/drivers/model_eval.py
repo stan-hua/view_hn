8baf5542809e2734fe089d4d0eba377c60916f98
Stanley Z. Hua <stanley.hua@mail.utoronto.ca>
2022-08-01 10:46:10 -0400

    Add option to choose between binary and multi-class model type

--- src/drivers/model_eval.py
+++ src/drivers/model_eval.py
@@ -4,13 +4,17 @@ model_eval.py
 Description: Used to evaluate a trained model's performance on the testing set.
 """
 
+# Standard libraries
+import logging
+import os
+
 # Non-standard libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 import torch
 import torchvision.transforms as T
+import yaml
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from torchvision.io import read_image, ImageReadMode
 
@@ -21,26 +25,34 @@ from src.data_prep.utils import load_metadata, extract_data_from_filename
 from src.models.efficientnet_pl import EfficientNetPL
 
 
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
+
 ################################################################################
 #                                  Constants                                   #
 ################################################################################
+LOGGER = logging.getLogger(__name__)
+
 # Flag to use GPU or not
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # Checkpoint for a trained 5-view model
-CKPT_PATH = constants.DIR_RESULTS + "/five_view/0/last.ckpt"
+CKPT_PATH_MULTI= constants.DIR_RESULTS + "/five_view/0/epoch=6-step=1392.ckpt"
 
 # Checkpoint for a trained binary-classifier model
-CKPT_PATH_BINARY = constants.DIR_RESULTS + "/binary_classifier/0/last.ckpt"
+CKPT_PATH_BINARY = constants.DIR_RESULTS + \
+    "/binary_classifier/0/epoch=14-step=2984.ckpt"
 
 # Table containing predictions and labels for test set
-TEST_PRED_PATH = constants.DIR_RESULTS + "/test_set_results(five_view).csv"
+TEST_PRED_PATH = constants.DIR_RESULTS + "/test_set_results(%s).csv"
 
+# Type of models (5 view, binary)
+MODEL_TYPES = ("five_view", "binary")
 
 ################################################################################
 #                                  Functions                                   #
 ################################################################################
-def get_test_set_metadata(df_metadata, dir=constants.DIR_IMAGES):
+def get_test_set_metadata(df_metadata, hparams, dir=constants.DIR_IMAGES):
     """
     Get metadata table containing (filename, label) for each image in the test
     set.
@@ -49,6 +61,8 @@ def get_test_set_metadata(df_metadata, dir=constants.DIR_IMAGES):
     ----------
     df_metadata : pandas.DataFrame
         Each row contains metadata for an ultrasound image.
+    hparams : dict
+        Hyperparameters used in model training run, used to load exact test set.
     dir : str, optional
         Path to directory containing metadata, by default constants.DIR_IMAGES
 
@@ -58,13 +72,6 @@ def get_test_set_metadata(df_metadata, dir=constants.DIR_IMAGES):
         Metadata of each image in the test set
     """
     # Set up data
-    hparams = {
-        "img_size": constants.IMG_SIZE,
-        "train": True,
-        "test": True,
-        "train_test_split": 0.75,
-        "train_val_split": 0.75
-    }
     dm = UltrasoundDataModule(df=df_metadata, dir=dir, **hparams)
     dm.setup()
 
@@ -105,7 +112,7 @@ def predict_on_images(model, filenames, dir=constants.DIR_IMAGES):
     # Predict on each images one-by-one
     with torch.no_grad():
         preds = []
-        for filename in filenames:
+        for i, filename in enumerate(filenames):
             img_path = filename if dir is None else f"{dir}/{filename}"
 
             # Load image as expected by model
@@ -124,8 +131,11 @@ def predict_on_images(model, filenames, dir=constants.DIR_IMAGES):
 
             # Convert from encoded label to label name
             pred_label = constants.IDX_TO_CLASS[pred]
-
             preds.append(pred_label)
+
+            # Log progress
+            LOGGER.info("Test Set Inference: %i%",
+                        (int(100 * (i + 1) / len(filenames))))
 
     return np.array(preds)
 
@@ -176,21 +186,70 @@ def transform_image(img):
     return transforms(img)
 
 
+def get_hyperparameters(hparam_dir=None, filename="hparams.yaml"):
+    """
+    Load hyperparameters from model training directory. If not provided, return
+    default hyperparameters.
+
+    Parameters
+    ----------
+    hparam_dir : str
+        Path to model training directory containing hyperparameters.
+    filename : str
+        Filename of YAML file with hyperparameters, by default "hparams.yaml"
+
+    Returns
+    -------
+    dict
+        Hyperparameters
+    """
+    if hparam_dir:
+        with open(os.path.join(hparam_dir, filename), "r") as stream:
+            try:
+                hparams = yaml.full_load(stream)
+                return hparams
+            except yaml.YAMLError as exc:
+                LOGGER.critical(exc)
+                LOGGER.critical("Using default hyperparameters...")
+
+    # If above does not succeed, use default hyperparameters
+    hparams = {
+        "img_size": constants.IMG_SIZE,
+        "train": True,
+        "test": True,
+        "train_test_split": 0.75,
+        "train_val_split": 0.75
+    }
+
+    return hparams
+
+
 ################################################################################
 #                                  Main Flows                                  #
 ################################################################################
-def main_test_set():
+def main_test_set(checkpoint_path=CKPT_PATH_MULTI, save_path=TEST_PRED_PATH):
     """
     Performs inference on test set, and saves results
-    """
-    # 1. Get image filenames and labels
-    df_metadata = load_metadata()
 
+    Parameters
+    ----------
+    checkpoint_path : str, optional
+        Path to file containing EfficientNetPL model checkpoint, by default
+        CKPT_PATH_MULTI
+    save_path : str, optional
+        Path to file to save test results to, by default TEST_PRED_PATH
+    """
     # 2. Get metadata, specifically for the test set
-    df_test_metadata = get_test_set_metadata(df_metadata)
+    # 2.0 Get image filenames and labels
+    df_metadata = load_metadata()
+    # 2.1 Get hyperparameters for run
+    model_dir = os.path.dirname(checkpoint_path)
+    hparams = get_hyperparameters(model_dir)
+    # 2.2 Load test metadata
+    df_test_metadata = get_test_set_metadata(df_metadata, hparams)
 
     # 3. Load existing model and send to device
-    model = EfficientNetPL.load_from_checkpoint(CKPT_PATH)
+    model = EfficientNetPL.load_from_checkpoint(checkpoint_path)
     model = model.to(DEVICE)
 
     # 4. Get predictions on test set
@@ -199,12 +258,23 @@ def main_test_set():
 
     # 5. Save predictions on test set
     df_test_metadata["pred"] = preds
-    df_test_metadata.to_csv(TEST_PRED_PATH, index=False)
+    df_test_metadata.to_csv(save_path, index=False)
 
 
 if __name__ == '__main__':
-    if os.path.exists(TEST_PRED_PATH):
-        df_test_metadata = pd.read_csv(TEST_PRED_PATH)
+    model_type = "binary"
+    assert model_type in MODEL_TYPES
 
-    # Plot confusion matrix
-    plot_confusion_matrix(df_test_metadata)
+    # Add model type to save path
+    test_save_path = TEST_PRED_PATH % (model_type,)
+
+    # Inference on test set
+    main_test_set(CKPT_PATH_MULTI if model_type == "five_view" \
+        else CKPT_PATH_BINARY, test_save_path)
+
+    # Load test metadata
+    if os.path.exists(test_save_path):
+        df_test_metadata = pd.read_csv(test_save_path)
+
+        # Plot confusion matrix
+        plot_confusion_matrix(df_test_metadata)
