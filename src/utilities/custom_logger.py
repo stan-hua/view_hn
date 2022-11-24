f9628d6128f577d21d95db1ec55cbb3b4d33bdd6
Stanley Z. Hua <stanley.hua@mail.utoronto.ca>
2022-11-23 20:34:52 -0500

    Fix FriendlyCSVLogger

--- src/utilities/custom_logger.py
+++ src/utilities/custom_logger.py
@@ -19,19 +19,40 @@ class FriendlyCSVLogger(CSVLogger):
     def finalize(self, status=None) -> None:
         metrics_path = os.path.join(self.experiment.metrics_file_path)
 
-        def _collapse_epoch(df_):
-            """Collapse epoch results to include all non-NA values"""
-            row = pd.melt(df_).dropna().drop_duplicates().rename(columns={"variable": "index"})
-            return row.set_index("index").T.reset_index(drop=True)
+        def collapse_epoch(df):
+            """
+            Collapse epoch results to one row
 
-        if os.path.exists(metrics_path):
-            df = pd.read_csv(metrics_path)
-            df = df.drop(columns=["step"])
-            df = df.groupby(by=['epoch'], as_index=False).apply(lambda df_: _collapse_epoch(df_))
+            Parameters
+            ----------
+            df : pandas.DataFrame
+                Results from the same epoch, across rows with null values
 
-            for col in df.columns.tolist():
-                if "_" not in col or "loss" in col:
-                    continue
-                df[col] = (df[col] * 100).round(decimals=2)
+            Returns
+            -------
+            pandas.DataFrame
+                Epoch results in one row
+            """
+            # Flatten structure
+            all_values = pd.melt(df).dropna().drop_duplicates(
+                subset="variable", keep="last")
+            # Reorganize back to row
+            row = all_values.set_index("variable").T
+            # Remove extra index names added
+            row = row.reset_index(drop=True)
+            row.columns.name = None
+            return row
 
-            df.to_csv(os.path.join(self.log_dir, "history.csv"), index=False)
+        if not os.path.exists(metrics_path):
+            return
+
+        df = pd.read_csv(metrics_path)
+        df = df.drop(columns=["step"])
+        df = df.groupby(by=['epoch'], as_index=False).apply(collapse_epoch)
+
+        for col in df.columns.tolist():
+            if "_" not in col or "loss" in col:
+                continue
+            df[col] = (df[col] * 100).round(decimals=2)
+
+        df.to_csv(os.path.join(self.log_dir, "history.csv"), index=False)
