833644833ceaaf2494db2289b17db04dee5a7d30
Stanley Hua <shua@data32.ccm.sickkids.ca>
2024-07-03 17:05:59 -0400

    Update configs

---
+++ slurm/submit_job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH --job-name=stans_job              # Job name
+#SBATCH --nodes=1                         # Number of nodes
+#SBATCH --gres=gpu:1                      # Request one GPU
+#SBATCH --cpus-per-task=6                 # Number of CPU cores per task
+#SBATCH --memory=16GB
+#SBATCH -o slurm-%j.out
+
+# If you want to do it in the terminal,
+# salloc --job-name=stans_job --nodes=1 --gres=gpu:1 --cpus-per-task=6
+
+# Load any necessary modules or activate your virtual environment here
+# For example:
+conda activate view
+
+# Run your Python script
+python -m src.scripts.model_training -c "train_sup_linear.ini"
