afbc45307c1d8ef554939705c207d51e5021e09d
Stanley Hua <shua@data32.ccm.sickkids.ca>
2024-07-04 13:11:55 -0400

    Update SLURM batch script

--- slurm/submit_job.sh
+++ slurm/submit_job.sh
@@ -3,11 +3,12 @@
 #SBATCH --nodes=1                         # Number of nodes
 #SBATCH --gres=gpu:1                      # Request one GPU
 #SBATCH --cpus-per-task=6                 # Number of CPU cores per task
-#SBATCH --memory=16GB
+#SBATCH --mem=32GB
 #SBATCH -o slurm-%j.out
 
 # If you want to do it in the terminal,
-# salloc --job-name=stans_job --nodes=1 --gres=gpu:1 --cpus-per-task=6
+# salloc --job-name=stans_terminal --nodes=1 --gres=gpu:1 --cpus-per-task=6 --mem=32GB -o slurm-%j.out
+# srun (command)
 
 # Load any necessary modules or activate your virtual environment here
 # For example:
