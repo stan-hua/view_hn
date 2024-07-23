bbfa77be1de533024ed3856c8be2e9b10db58b7b
Stanley Hua <shua@data32.ccm.sickkids.ca>
2024-07-23 15:36:35 -0400

    Update SSL eval script for passing hyperparameters

---
+++ slurm/submit_ssl_eval_job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH --job-name=ssl_train_and_eval     # Job name
+#SBATCH --nodes=1                         # Number of nodes
+#SBATCH --gres=gpu:1                      # Request one GPU
+#SBATCH --cpus-per-task=6                 # Number of CPU cores per task
+#SBATCH --mem=32GB
+#SBATCH -o slurm/logs/slurm-%j.out
+
+# If you want to do it in the terminal,
+# salloc --job-name=stans_terminal --nodes=1 --gres=gpu:1 --cpus-per-task=6 --mem=32GB
+# srun (command)
+
+# Load any necessary modules or activate your virtual environment here
+micromamba activate view
+
+# Evaluate model
+srun python -m src.scripts.ssl_model_eval \
+    --exp_name "exp_ssl_pretrain-byol" \
+    --dset "val"
+
